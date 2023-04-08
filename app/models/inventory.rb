e978ba0289c180bf348f454e0f0bf79a91598070
evandrotvc <evandro-tvc@hotmail.com>
2023-04-08 17:46:32 -0300

    feat: create callback in item model and methods check if exist items in inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -4,4 +4,12 @@ class Inventory < ApplicationRecord
   belongs_to :user
 
   has_many :items, dependent: :destroy
+
+  def all_items_in_inventory?(items_array)
+    items.where(kind: items_array).count == items_array.count
+  end
+
+  def teste(items_array)
+    byebug
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,26 @@
 class Item < ApplicationRecord
   belongs_to :inventory
 
-  validates :kind, :point, presence: true
+  validates :kind, presence: true
 
   enum kind: {
     water: 'water', food: 'food', remedy: 'remedy', ammunition: 'ammunition'
   }
 
+  before_create :set_points
+
+  private
+
+  def set_points
+    case kind
+    when "water"
+      self.point = 4
+    when "food"
+      self.point =  3
+    when "remedy"
+      self.point =  2
+    else
+      self.point =  1
+    end
+  end
 end
